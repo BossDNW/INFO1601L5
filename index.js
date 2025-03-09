485a094c92b506ed75dff4f08c4c044332490e49
D'mari Williams <dmariw90@outlook.com>
2025-03-09 17:40:03 -0400

    Task 3

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
+let name = "Bob";
+let age = 24;
 
-for(let i=0; i< arr.length; i++){
-  console.log(arr[i])
-}
+console.log(typeof(name))
+console.log(typeof(age))
+
+console.log(`Hello my name is ${name}, I'm ${age} years old`)
+console.log(`I was born in ${2025-age}`)
